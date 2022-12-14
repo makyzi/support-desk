ddf2f492ea8f305687013d685af583fc6833f6f3
makyzi <sedlacek.martin9@gmail.com>
2022-12-14 16:42:07 +0100

    backend work

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,28 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 
-const PORT = 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
+
+const connectDB = require('./config/db')
+
+// Connect DB
+connectDB()
+
+const PORT = process.env.PORT || 8000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Welcome to Support Desk API' })
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
